5cde1965dda1bd851e326749b9064d37e1b7dd53
Sanjay Kumar SS <sanjay.savitha05@gmail.com>
2026-02-19 21:50:45 +0530

    test

--- backend/api/v1/endpoints/crud.py
+++ backend/api/v1/endpoints/crud.py
@@ -4,12 +4,26 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
+
+from models.crud import Product
+from schemas.crud import ProductCreate, ProductResponse
+
 load_dotenv()
 
 
-engine = create_engine(os.getenv.DATABASE_URL)
+engine = create_engine(os.getenv("DATABASE_URL"))
 SessionLocal = sessionmaker(bind = engine)
 
 crudRouter = APIRouter()
 
-# @crudRouter.
+@crudRouter.post("/products", response_model=ProductResponse)
+def create_product(product: ProductCreate):
+    db = SessionLocal()
+
+    db_product = Product(**product.dict())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+
+    db.close()
+    return db_product
