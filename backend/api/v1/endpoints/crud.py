23cec2ca7161770882351becee1dd80235203b3a
Sanjay Kumar SS <sanjay.savitha05@gmail.com>
2026-02-19 12:52:40 +0530

    test

--- backend/api/v1/endpoints/crud.py
+++ backend/api/v1/endpoints/crud.py
@@ -1,4 +1,14 @@
 from fastapi import APIRouter, HTTPException, status
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+engine = create_engine(os.getenv.DATABASE_URL)
+SessionLocal = sessionmaker(bind = engine)
 
 crudRouter = APIRouter()
 
