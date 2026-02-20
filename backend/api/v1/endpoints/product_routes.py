1ad8ec6df0bc60f0a2773f616d221f5a38cd782e
Sanjay Kumar SS <sanjay.savitha05@gmail.com>
2026-02-20 11:20:33 +0530

    fix: models and schema mismatch and add_new_products route

--- backend/api/v1/endpoints/product_routes.py
+++ backend/api/v1/endpoints/product_routes.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter, HTTPException, status
+from pydantic import ValidationError
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
 
-from models.product_models import ProductCreate, ProductCreateResponse
+
+from schemas.product_schemas import ProductCreate, ProductCreateResponse
+from models.product_models import Product
 from lib.database import SessionLocal
-from schemas.product_schemas import Product
+
 product_router = APIRouter()
 
 @product_router.get("/health", status_code=status.HTTP_200_OK)
@@ -10,16 +15,24 @@ def check_health():
     return {"status", "ok"}
 
 # add new products to the store
-@product_router.post("/add", status_code=status.HTTP_201_CREATED, response_model = ProductCreateResponse)
+@product_router.post(
+        "/add",
+        status_code=status.HTTP_201_CREATED,
+        response_model = ProductCreateResponse
+)
 def add_new_products(product: ProductCreate):
+    db: Session = SessionLocal()
     try:
-        db_session = SessionLocal()
+        db = SessionLocal()
         db_product = Product(**product.dict())
-        db_session.add(db_product)
-        db_session.commit()
-        db_session.refresh(db_product)
-        db_session.close()
-        
+        db.add(db_product)
+        db.commit()
+        db.refresh(db_product)
+
         return db_product
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail= str(e))
+    
+    except SQLAlchemyError as sqla_e:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail= f"Error with SQLAlchemy: {sqla_e}")
+    finally:
+        db.close()
